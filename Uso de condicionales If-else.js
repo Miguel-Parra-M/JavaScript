a061d1f6c8d4413e521978adbeabd2390b4735b5
Miguel Parra <pmjm0990@gmail.com>
2021-04-16 05:12:31 -0500

    Uso de los tipos de datos y las sentencias If-else

---
+++ Uso de condicionales If-else.js
@@ -0,0 +1,36 @@
+// Ejercicio 10
+    
+let Distancia, condicion;
+Distancia= 120;
+
+// Semi-condicionales
+
+let condicion = Distancia > 100;
+console.log (condicion);
+
+
+//Condicional simple
+if (Distancia > 100){
+
+    console.log ( "La distancia es mayor a 100"); 
+}
+
+//condicional compuesta
+if (Distancia < 100){
+
+    console.log ( "La distancia es menor a 100");
+}
+else{
+
+    console.log ( "La distancia es mayor a 100");
+}
+
+// Condicional compuesta multiple
+if (Distancia < 100){
+console.log ( "La distancia es menor a 100");
+}
+else if(Distancia > 100){
+console.log ("La distancia es mayor a 100");
+}else{
+    console.log ("No ingreso un dato correcto")
+}
