a4c049cc8f229e5f4737ebcaa6b812b9e7b37efc
Miguel Parra <pmjm0990@gmail.com>
2021-04-18 11:19:22 -0500

    Uso de las condicionales For y Switch

---
+++ Uso de condicional Switch.js
@@ -0,0 +1,24 @@
+// Ejercicio 11
+    
+    let nota = "D";
+    // Condicional de casos switch
+    switch (nota) {
+        case "A": 
+            console.log ("Muy bien !!");         
+            break;
+
+        case "B":
+            console.log ("Bien !?");
+            break;
+        
+        case "C":
+            console.log ("Mal !!")
+            break;
+        
+        default:
+            console.log (" Ingreso un dato incorrecto a la escala de valoraci?n");
+            break;
+    
+    }
+
+    
