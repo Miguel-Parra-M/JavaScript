6f8444dac90be23330986f703054ff059ade6552
Miguel Parra <pmjm0990@gmail.com>
2021-07-10 10:15:36 -0500

    Condicional Switch

---
+++ Condicional Switch.js
@@ -0,0 +1,20 @@
+// Switch - Condicional de casos 
+let nota = "D";
+
+switch (nota) {
+    case "A": 
+        console.log ("Muy bien !!");         
+        break;
+
+    case "B":
+        console.log ("Bien !?");
+        break;
+    
+    case "C":
+        console.log ("Mal !!")
+        break;
+    
+    default:
+        console.log (" Ingreso un dato incorrecto a la escala de valoración");
+        break;
+}
